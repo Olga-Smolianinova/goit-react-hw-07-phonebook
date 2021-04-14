4e716bcf680b9a31be0d54c93b6f874a5eaccd38
Olga-Smolianinova <olga.smol2015@gmail.com>
2021-04-14 11:32:12 +0300

    initial settings

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux'; //Для подключение  глобального store  к Redux. Достаем  Provider  (пакет из react-redux) - компонент, который оборачивает все наше приложение, он ставится поверх App, и он через контекст (контекст - это глобальная переменная) может прокидывать в глубину и на любую вложенность дополнительный функционал.
 
+import { BrowserRouter } from 'react-router-dom';
+
 // import { PersistGate } from 'redux-persist/integration/react'; // используется для реализации redux-persist
 
 // Data
@@ -25,7 +27,10 @@ ReactDOM.render(
     <Provider store={store}>
       {/*PersistGate используется для реализации redux-persist. В props передаем 1)loading, в который можно указать какой-либо preloader; 2) persistor  - ссылка на сам persistor, который заимпортирован из файла store.js*/}
       {/* <PersistGate loading={null} persistor={store.persistor}> */}
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+
       {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>,
